7d7b94455df324c00bec198ad8cd8accb7921aef
xjkdev <xjk2008@gmail.com>
2015-04-28 22:32:03 +0800

    add some useful function.

--- whiteboard/MainWindow.xaml.cs
+++ whiteboard/MainWindow.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using System.Drawing.Imaging;
+//using System.Drawing.Imaging;
 //using System.Drawing.Graphics;
 using System.Windows.Threading;
 
@@ -25,6 +25,54 @@ namespace whiteboard {
         public MainWindow() {
             InitializeComponent();
         }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e) {
+            //DrawingVisual dv = new DrawingVisual();
+            
+        }
+
+        private void inkcanvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e) {
+            
+        }
+
+        private void inkcanvas_PreviewTouchDown(object sender, TouchEventArgs e) {
+            //tlabal1.Content = e.Timestamp.ToString();
+            //tlabal1.Content = inkcanvas.Children.Count.ToString();
+            
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) {
+            if (inkcanvas.EditingMode == InkCanvasEditingMode.Ink) {
+                inkcanvas.EditingMode = InkCanvasEditingMode.EraseByStroke;
+                penbtn.Content = "书写";
+                
+            } else {
+                inkcanvas.EditingMode = InkCanvasEditingMode.Ink;
+                penbtn.Content = "擦除";
+            }
+        }
+
+        private void colorbtn_Click(object sender, RoutedEventArgs e) {
+            if (inkcanvas.DefaultDrawingAttributes.Color == Colors.Black) {
+                inkcanvas.DefaultDrawingAttributes.Color = Colors.Blue;
+                colorbtn.Content = "黑笔";
+            } else {
+                inkcanvas.DefaultDrawingAttributes.Color = Colors.Black;
+                colorbtn.Content = "蓝笔";
+            }
+        }
+
+        private void undobtn_Click(object sender, RoutedEventArgs e) {
+            if (inkcanvas.Strokes.Count>0)
+                inkcanvas.Strokes.RemoveAt(inkcanvas.Strokes.Count - 1);
+        }
+
+        private void clearbtn_Click(object sender, RoutedEventArgs e) {
+            inkcanvas.Strokes.Clear();
+        }
+
+
 
     }
 }
